b9c76a0eedbfeed4bb9c9a6126a0d19f8a850a14
sylee1999 <125670383+sylee1999@users.noreply.github.com>
2023-09-29 19:52:45 +0900

    Update 0929 GRU.py

--- 0929 GRU.py
+++ 0929 GRU.py
@@ -1,3 +1,6 @@
+# 설치
+# pip install keras-tuner
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from simhash import Simhash
@@ -26,6 +29,8 @@ class ForensicsDataPreprocessing:
             features = df.iloc[0, 1:-1].values
             df.columns = ['name'] + list(features) + ['label']
             df = df[1:]
+
+
         else:
             features = df.iloc[0, 1:-1].values
             df.columns = ['name'] + list(features) + ['label']
@@ -56,9 +61,11 @@ class ForgeryDetectorEngine:
         y = df['label'].values
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
+
         # 변환안하니까 오류나네
         y_train = pd.to_numeric(y_train, errors='coerce').astype(np.int32)
         y_test = pd.to_numeric(y_test, errors='coerce').astype(np.int32)
+
 
         # 모델 구성
         self.model = Sequential()
@@ -67,9 +74,14 @@ class ForgeryDetectorEngine:
         self.model.add(Dense(4, activation='softmax'))
         self.model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+
+
         checkpoint = ModelCheckpoint('model.h5', save_best_only=True, monitor='val_accuracy', mode='max')
         self.model.fit(X_train[:, :, None], y_train, validation_data=(X_test[:, :, None], y_test), epochs=10,
                        batch_size=32, callbacks=[checkpoint])
+
+
+
         # 성능
         loss, accuracy = self.model.evaluate(X_test[:, :, None], y_test)
         print("Model accuracy:", accuracy)
@@ -100,13 +112,18 @@ class ForgeryDetectorEngine:
         for name, avg in group_averages.items():
             original_label = original_labels[original_labels['name'] == name]['label'].values[0]
             closest_label = round(avg)
-
             results[name] = f'기존 label : {original_label}, 예측 label : {closest_label}'
+
+
             if int(original_label) == closest_label:
                 success_failure[name] = "예측 성공"
             else:
                 success_failure[name] = "예측 실패"
-        return results, success_failure
+
+        results_df = pd.DataFrame(list(results.items()), columns=['name', 'result'])
+
+
+        return results, success_failure, results_df
 
 
 if __name__ == "__main__":
@@ -125,6 +142,8 @@ if __name__ == "__main__":
 
 
     #####
+
+
     df_test_melted, original_labels = data_preprocessor.preprocess_data('dataset-2.csv', is_train=False)
     df_test_melted = data_preprocessor.apply_simhash(df_test_melted)
 
@@ -133,39 +152,16 @@ if __name__ == "__main__":
     predicted_data = detector_engine.predict_data(df_test_melted)
     predicted_data.to_csv("result2.csv", index=False)
 
-    print(predicted_data[['name', 'label']].head())
 
 
+    results, success_failure, results_df = detector_engine.analyze_prediction(predicted_data, original_labels)
 
-    results, success_failure = detector_engine.analyze_prediction(predicted_data, original_labels)
 
-    print(results)
     print(success_failure)
-    ###
-    """
-    print("Result : ")
-    for name, result in results.items():
-        print(f"The file {name}  {result}.")
+    print(results_df)
 
 
-    y_test = df_test_melted['label'].values
-
-    fpr, tpr, _ = roc_curve(y_test, predicted_data['label'])
-    roc_auc = auc(fpr, tpr)
-
-    plt.figure()
-
-
-    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-
-
-    plt.title('ROC')
-    plt.legend(loc='lower right')
-    plt.show()
-    print(f'AUC Score: {roc_auc:.2f}')
-    """
+    total = len(results_df)
+    success = sum([1 for row in success_failure.values() if "예측 성공" in row])
+    success_rate = (success / total) * 100
+    print(f"예측 성공률: {success_rate:.2f}%")
